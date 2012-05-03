6746291c2ff41537b664128d91468b59a19b3d33
Scott Milliken <scott@themillikens.com>
2012-05-02 23:14:38 -0400

    Modified the create.sql file and added a db-1.0-to-1.1.sql file for migrating from previous version.  No structures were changed in the database, but the DELL_ID parameter was removed, and parameters for SMTPUser and SMTPPAssword were added so that authenticated SMTP could be supported.

---
+++ db-1.0-to-1.1.sql
@@ -0,0 +1,13 @@
+--
+-- Dell changed their web page and broke the warranty information retrieval script, so the
+-- use of this parameter has been deprecated.
+--
+
+delete from fac_Config where Parameter='DELL_ID';
+
+--
+-- Added SMTPUser and SMTPPassword so that authentication can be used for sending email
+--
+
+insert into fac_Config values ('SMTPUser','','Username','string',''),
+	('SMTPPassword','','Password','string','');
