26b03f012241f108f8ab317d8c1b248ee2945420
wilpig <wilbur@wilpig.org>
2012-07-10 06:51:33 -0500

    Changing date validation from YYYY-DD-MM to MM-DD-YYYY

--- scripts/jquery.validationEngine-en.js
+++ scripts/jquery.validationEngine-en.js
@@ -95,8 +95,8 @@
                     "alertText": "* Invalid floating decimal number"
                 },
                 "date": {
-                    "regex": /^\d{4}[\/\-](0?[1-9]|1[012])[\/\-](0?[1-9]|[12][0-9]|3[01])$/,
-                    "alertText": "* Invalid date, must be in YYYY-MM-DD format"
+					"regex": /^(0?[1-9]|1[012])[\/\-](0?[1-9]|[12][0-9]|3[01])[\/\-]\d{4}$/,
+					"alertText": "* Invalid date, must be in MM-DD-YYYY format"
                 },
                 "ipv4": {
                     "regex": /^((([01]?[0-9]{1,2})|(2[0-4][0-9])|(25[0-5]))[.]){3}(([0-1]?[0-9]{1,2})|(2[0-4][0-9])|(25[0-5]))$/,
