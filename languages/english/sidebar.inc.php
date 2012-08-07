1a1e4736c25ef05056667be81fad72738bd712bc
wilpig <wilbur@wilpig.org>
2012-08-07 06:21:41 -0500

    Example for future language file format options #35

---
+++ languages/english/sidebar.inc.php
@@ -0,0 +1,29 @@
+<?php
+
+$txt['searchbyname']="Search by Name";
+$txt['searchbysn']="Search by SN";
+$txt['searchbyassettag']="Search by Asset Tag";
+
+$txt['reports']="Reports";
+
+$txt['rackrequest']="Rack Request Form";
+
+$txt['contactadmin']="Contact Administration";
+$txt['deptadmin']="Dept. Admininistration";
+$txt['timeperiods']="Time Periods";
+$txt['escrules']="Escalation Rules";
+
+$txt['editcab']="Edit Cabinets";
+$txt['edittemp']="Edit Templates";
+
+$txt['manageusers']="Manage Users";
+$txt['managesupplybins']="Manage Supply Bins";
+$txt['managesupplies']="Manage Supplies";
+$txt['datacenters']="Edit Data Centers";
+$txt['powersources']="Edit Power Sources";
+$txt['panalmgr']="Edit Power Panels";
+$txt['manuf']="Edit Manufacturers";
+$txt['conf']="Edit Configuration";
+
+$txt['home']="Home";
+?>
