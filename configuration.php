10abc4d95813cff7dace83235f706018ab0ac85e
wilpig <wilbur@wilpig.org>
2012-08-26 08:19:07 -0500

    New configuration screen

--- configuration.php
+++ configuration.php
@@ -25,10 +25,20 @@
 		}
 		$config->UpdateConfig($facDB);
 	}
-	if(isset($_REQUEST["Revert"]) && $_REQUEST["Revert"]=="yes"){
-		$config->RevertToDefault($facDB,$_REQUEST["Single"]);
-		$config=new Config($facDB);
+
+	// make list of department types
+	$i=0;
+	$classlist="";
+	foreach($config->ParameterArray["ClassList"] as $item){
+		$classlist .= $item;
+		if($i+1 != count($config->ParameterArray["ClassList"])){
+			$classlist.=", ";
+		}
+		$i++;
 	}
+
+
+
 ?>
 <html>
 <head>
@@ -37,29 +47,49 @@
   <title>openDCIM Data Center Inventory</title>
   <link rel="stylesheet" href="css/inventory.php" type="text/css">
   <link rel="stylesheet" href="css/jquery.miniColors.css" type="text/css">
+  <link rel="stylesheet" href="css/jquery-ui-1.8.18.custom.css" type="text/css">
   <!--[if lt IE 9]>
   <link rel="stylesheet"  href="css/ie.css" type="text/css">
   <![endif]-->
   
   <script type="text/javascript" src="scripts/jquery.min.js"></script>
+  <script type="text/javascript" src="scripts/jquery-ui-1.8.18.custom.min.js"></script>
   <script type="text/javascript" src="scripts/jquery.miniColors.js"></script>
   <script type="text/javascript">
-	function verifyRevert(formname, para) {
-		if (confirm( "Do you really want to revert configuration to factory default?" )){
-		document.getElementById('Revert').value='yes';
-		document.getElementById('Single').value=para;
-		formname.submit();
-		}else{
-		document.getElementById('Revert').value='no';
-		formname.submit();
-		}
-	}
 	$(document).ready( function() {
 		$(".color-picker").miniColors({
 			letterCase: 'uppercase',
-			change: function(hex, rgb) {
-				logData(hex, rgb);
-			}
+			change: function(hex, rgb){}
+		});
+		$("#configtabs").tabs();
+		$("#configtabs input[defaultvalue]").each(function(){
+			$(this).parent().after('<div><button type="button">&lt;--</button></div><div><span>'+$(this).attr('defaultvalue')+'</span></div>');
+		});
+		$("#configtabs button").each(function(){
+			var a = $(this).parent().prev().find('input');
+			$(this).click(function(){
+				a.triggerHandler("focus");
+				a.val($(this).parent().next().children('span').text());
+				a.triggerHandler("paste");
+				a.focus();
+				a.setSelectionRange(a.val().length, a.val().length);
+				$('input[name="OrgName"]').focus();
+				$('input[name="OrgName"]').setSelectionRange(0, 0);
+//				setTimeout(function () {$(this).parent().next().children('span').triggerHandler('click');}, 50);
+//				$(this).parent().prev().find('input').blur();
+			});
+		});
+		$('input[name="HeaderColor"]').blur(function(){
+			$('#header').css('background-color',$(this).val());
+		});
+		$('input[name="BodyColor"]').blur(function(){
+			$('.main').css('background-color',$(this).val());
+		});
+		$('input[name="LinkColor"]').blur(function(){
+			$("head").append("<style type=\"text/css\">a:link, a:hover, a:visited:hover {color: "+$(this).val()+";}</style>");
+		});
+		$('input[name="VisitedLinkColor"]').blur(function(){
+			$("head").append("<style type=\"text/css\">a:visited {color: "+$(this).val()+";}</style>");
 		});
 	});
 
@@ -70,68 +100,236 @@
 <div class="page config">
 <?php
 	include( "sidebar.inc.php" );
-?>
-<div class="main">
-<h2><?php print $config->ParameterArray["OrgName"]; ?></h2>
-<h3>Data Center Configuration</h3>
-<h3>Database Version:  <?php print $config->ParameterArray["Version"]; ?></h3>
+
+echo '<div class="main">
+<h2>',$config->ParameterArray["OrgName"],'</h2>
+<h3>',_("Data Center Configuration"),'</h3>
+<h3>',_("Database Version"),': ',$config->ParameterArray["Version"],'</h3>
 <div class="center"><div>
-<form action="<?php print $_SERVER["PHP_SELF"]; ?>" method="POST">
-   <input type="hidden" name="Revert" id="Revert" value="no">
-   <input type="hidden" name="Single" id="Single" value="none">
-   <input type="hidden" name="Version" value="<?php print $config->ParameterArray["Version"]; ?>">
-<div class="table rights">
-<div><div><h3>Parameter</h3></div><div><h3>Value</h3></div><div></div><div><h3>Default Value</h3></div></div>
-<?php
-	foreach ($config->ParameterArray as $key=>$value){
-	
-		if ( strpos( $key, "Color" ) ) {
-			$class='class="color-picker"';
-			$cssfix1='<div class="cp">';
-			$cssfix2='</div>';
-		} else { 
-			$cssfix1=$cssfix2=$class='';
-		}
-		
-		if ($key =="ClassList"){
-			$numItems=count($config->ParameterArray[$key]);
-			$i=0;
-			$valueStr="";
-			foreach($config->ParameterArray[$key] as $item){
-				$valueStr .= $item;
-				if($i+1 != $numItems){
-					$valueStr.=", ";
-				}
-				$i++;
-			}
-			print "<div>\n";
-			print "<div>$key:</div>\n";
-			print "<div><input type=\"text\" maxlength=\"200\" name=\"$key\" value=\"$valueStr\"></div>\n";
-			print "<div><input type=\"button\" value=\"Revert To Default\" onclick=\"javascript:verifyRevert(this.form,'$key')\"></div>\n";
-			print "<div>{$config->defaults[$key]}</div>\n";
-			print "</div>\n";
-		} elseif ( $key != "Version" ) {
-			print "<div>\n";
-			print "<div>$key:</div>\n";
-			print "<div>$cssfix1<input type=\"text\" $class maxlength=\"200\" name=\"$key\" value=\"{$config->ParameterArray[$key]}\">$cssfix2</div>\n";
-			print "<div><input type=\"button\" value=\"Revert To Default\" onclick=\"javascript:verifyRevert(this.form,'$key')\"></div>\n";
-			print "<div>{$config->defaults[$key]}</div>\n";
-			print "</div>\n";
-		}
-	}
+<form action="',$_SERVER["PHP_SELF"],'" method="POST">
+   <input type="hidden" name="Version" value="',$config->ParameterArray["Version"],'">
+
+	<div id="configtabs">
+		<ul>
+			<li><a href="#general">',_("General"),'</a></li>
+			<li><a href="#style">',_("Style"),'</a></li>
+			<li><a href="#email">',_("Email"),'</a></li>
+			<li><a href="#reporting">',_("Reporting"),'</a></li>
+		</ul>
+		<div id="general">
+			<div class="table">
+				<div>
+					<div><label for="OrgName">',_("Organization Name"),'</label></div>
+					<div><input type="text" defaultvalue="',$config->defaults["OrgName"],'" name="OrgName" value="',$config->ParameterArray["OrgName"],'"></div>
+				</div>
+				<div>
+					<div><label for="Locale">',_("Locale"),'</label></div>
+					<div><input type="text" defaultvalue="',$config->defaults["Locale"],'" name="Locale" value="',$config->ParameterArray["Locale"],'"></div>
+				</div>
+				<div>
+					<div><label for="DefaultPanelVoltage">',_("Default Panel Voltage"),'</label></div>
+					<div><input type="text" defaultvalue="',$config->defaults["DefaultPanelVoltage"],'" name="DefaultPanelVoltage" value="',$config->ParameterArray["DefaultPanelVoltage"],'"></div>
+				</div>
+			</div> <!-- end table -->
+			<h3>Users</h3>
+			<div class="table">
+				<div>
+					<div><label for="ClassList">',_("Department Types"),'</label></div>
+					<div><input type="text" defaultvalue="',$classlist,'" name="ClassList" value="',$classlist,'"></div>
+				</div>
+				<div>
+					<div><label for="UserLookupURL">',_("User Lookup URL"),'</label></div>
+					<div><input type="text" defaultvalue="',$config->defaults["UserLookupURL"],'" name="UserLookupURL" value="',$config->ParameterArray["UserLookupURL"],'"></div>
+				</div>
+			</div> <!-- end table -->
+			<h3>Rack Requests</h3>
+			<div class="table">
+				<div>
+					<div><label for="MailSubject">',_("Mail Subject"),'</label></div>
+					<div><input type="text" defaultvalue="',$config->defaults["MailSubject"],'" name="MailSubject" value="',$config->ParameterArray["MailSubject"],'"></div>
+				</div>
+				<div>
+					<div><label for="RackWarningHours">',_("Warning (Hours)"),'</label></div>
+					<div><input type="text" defaultvalue="',$config->defaults["RackWarningHours"],'" name="RackWarningHours" value="',$config->ParameterArray["RackWarningHours"],'"></div>
+				</div>
+				<div>
+					<div><label for="RackOverdueHours">',_("Critical (Hours)"),'</label></div>
+					<div><input type="text" defaultvalue="',$config->defaults["RackOverdueHours"],'" name="RackOverdueHours" value="',$config->ParameterArray["RackOverdueHours"],'"></div>
+				</div>
+			</div> <!-- end table -->
+			<h3>Rack Usage</h3>
+			<div class="table">
+				<div>
+					<div><label for="SpaceRed">',_("Space Critical"),'</label></div>
+					<div><input type="text" defaultvalue="',$config->defaults["SpaceRed"],'" name="SpaceRed" value="',$config->ParameterArray["SpaceRed"],'"></div>
+				</div>
+				<div>
+					<div><label for="SpaceYellow">',_("Space Warning"),'</label></div>
+					<div><input type="text" defaultvalue="',$config->defaults["SpaceYellow"],'" name="SpaceYellow" value="',$config->ParameterArray["SpaceYellow"],'"></div>
+				</div>
+				<div>
+					<div><label for="WeightRed">',_("Weight Critical"),'</label></div>
+					<div><input type="text" defaultvalue="',$config->defaults["WeightRed"],'" name="WeightRed" value="',$config->ParameterArray["WeightRed"],'"></div>
+				</div>
+				<div>
+					<div><label for="WeightYellow">',_("Weight Warning"),'</label></div>
+					<div><input type="text" defaultvalue="',$config->defaults["WeightYellow"],'" name="WeightYellow" value="',$config->ParameterArray["WeightYellow"],'"></div>
+				</div>
+				<div>
+					<div><label for="PowerRed">',_("Power Critical"),'</label></div>
+					<div><input type="text" defaultvalue="',$config->defaults["PowerRed"],'" name="PowerRed" value="',$config->ParameterArray["PowerRed"],'"></div>
+				</div>
+				<div>
+					<div><label for="PowerYellow">',_("Power Warning"),'</label></div>
+					<div><input type="text" defaultvalue="',$config->defaults["PowerYellow"],'" name="PowerYellow" value="',$config->ParameterArray["PowerYellow"],'"></div>
+				</div>
+			</div> <!-- end table -->
+		</div>
+		<div id="style">
+			<h3>Racks & Maps</h3>
+			<div class="table">
+				<div>
+					<div><label for="CriticalColor">',_("Critical Color"),'</label></div>
+					<div><div class="cp"><input type="text" class="color-picker" name="CriticalColor" value="',$config->ParameterArray["CriticalColor"],'"></div></div>
+					<div><button type="button"><--</button></div>
+					<div><span>',strtoupper($config->defaults["CriticalColor"]),'</span></div>
+				</div>
+				<div>
+					<div><label for="CautionColor">',_("Caution Color"),'</label></div>
+					<div><div class="cp"><input type="text" class="color-picker" name="CautionColor" value="',$config->ParameterArray["CautionColor"],'"></div></div>
+					<div><button type="button"><--</button></div>
+					<div><span>',strtoupper($config->defaults["CautionColor"]),'</span></div>
+				</div>
+				<div>
+					<div><label for="GoodColor">',_("Good Color"),'</label></div>
+					<div><div class="cp"><input type="text" class="color-picker" name="GoodColor" value="',$config->ParameterArray["GoodColor"],'"></div></div>
+					<div><button type="button"><--</button></div>
+					<div><span>',strtoupper($config->defaults["GoodColor"]),'</span></div>
+				</div>
+				<div>
+					<div>&nbsp;</div>
+					<div></div>
+					<div></div>
+					<div></div>
+				</div>
+				<div>
+					<div><label for="ReservedColor">',_("Reserved Devices"),'</label></div>
+					<div><div class="cp"><input type="text" class="color-picker" name="ReservedColor" value="',$config->ParameterArray["ReservedColor"],'"></div></div>
+					<div><button type="button"><--</button></div>
+					<div><span>',strtoupper($config->defaults["ReservedColor"]),'</span></div>
+				</div>
+				<div>
+					<div><label for="FreeSpaceColor">',_("Unused Spaces"),'</label></div>
+					<div><div class="cp"><input type="text" class="color-picker" name="FreeSpaceColor" value="',$config->ParameterArray["FreeSpaceColor"],'"></div></div>
+					<div><button type="button"><--</button></div>
+					<div><span>',strtoupper($config->defaults["FreeSpaceColor"]),'</span></div>
+				</div>
+			</div> <!-- end table -->
+			<h3>Site</h3>
+			<div class="table">
+				<div>
+					<div><label for="HeaderColor">',_("Header Color"),'</label></div>
+					<div><div class="cp"><input type="text" class="color-picker" name="HeaderColor" value="',$config->ParameterArray["HeaderColor"],'"></div></div>
+					<div><button type="button"><--</button></div>
+					<div><span>',strtoupper($config->defaults["HeaderColor"]),'</span></div>
+				</div>
+				<div>
+					<div><label for="BodyColor">',_("Body Color"),'</label></div>
+					<div><div class="cp"><input type="text" class="color-picker" name="BodyColor" value="',$config->ParameterArray["BodyColor"],'"></div></div>
+					<div><button type="button"><--</button></div>
+					<div><span>',strtoupper($config->defaults["BodyColor"]),'</span></div>
+				</div>
+				<div>
+					<div><label for="LinkColor">',_("Link Color"),'</label></div>
+					<div><div class="cp"><input type="text" class="color-picker" name="LinkColor" value="',$config->ParameterArray["LinkColor"],'"></div></div>
+					<div><button type="button"><--</button></div>
+					<div><span>',strtoupper($config->defaults["LinkColor"]),'</span></div>
+				</div>
+				<div>
+					<div><label for="VisitedLinkColor">',_("Viewed Link Color"),'</label></div>
+					<div><div class="cp"><input type="text" class="color-picker" name="VisitedLinkColor" value="',$config->ParameterArray["VisitedLinkColor"],'"></div></div>
+					<div><button type="button"><--</button></div>
+					<div><span>',strtoupper($config->defaults["VisitedLinkColor"]),'</span></div>
+				</div>
+			</div> <!-- end table -->
+		</div>
+		<div id="email">
+			<div class="table">
+				<div>
+					<div><label for="SMTPServer">',_("SMTP Server"),'</label></div>
+					<div><input type="text" defaultvalue="',$config->defaults["SMTPServer"],'" name="SMTPServer" value="',$config->ParameterArray["SMTPServer"],'"></div>
+				</div>
+				<div>
+					<div><label for="SMTPPort">',_("SMTP Port"),'</label></div>
+					<div><input type="text" defaultvalue="',$config->defaults["SMTPPort"],'" name="SMTPPort" value="',$config->ParameterArray["SMTPPort"],'"></div>
+				</div>
+				<div>
+					<div><label for="SMTPHelo">',_("SMTP Helo"),'</label></div>
+					<div><input type="text" defaultvalue="',$config->defaults["SMTPHelo"],'" name="SMTPHelo" value="',$config->ParameterArray["SMTPHelo"],'"></div>
+				</div>
+				<div>
+					<div><label for="SMTPUser">',_("SMTP Username"),'</label></div>
+					<div><input type="text" defaultvalue="',$config->defaults["SMTPUser"],'" name="SMTPUser" value="',$config->ParameterArray["SMTPUser"],'"></div>
+				</div>
+				<div>
+					<div><label for="SMTPPass">',_("SMTP Password"),'</label></div>
+					<div><input type="text" defaultvalue="',$config->defaults["SMTPPass"],'" name="SMTPPass" value="',$config->ParameterArray["SMTPPass"],'"></div>
+				</div>
+				<div>
+					<div><label for="MailToAddr">',_("Mail To"),'</label></div>
+					<div><input type="text" defaultvalue="',$config->defaults["MailToAddr"],'" name="MailToAddr" value="',$config->ParameterArray["MailToAddr"],'"></div>
+				</div>
+				<div>
+					<div><label for="MailFromAddr">',_("Mail From"),'</label></div>
+					<div><input type="text" defaultvalue="',$config->defaults["MailFromAddr"],'" name="MailFromAddr" value="',$config->ParameterArray["MailFromAddr"],'"></div>
+				</div>
+				<div>
+					<div><label for="ComputerFacMgr">',_("Facility Manager"),'</label></div>
+					<div><input type="text" defaultvalue="',$config->defaults["ComputerFacMgr"],'" name="ComputerFacMgr" value="',$config->ParameterArray["ComputerFacMgr"],'"></div>
+				</div>
+				<div>
+					<div><label for="FacMgrMail">',_("Facility Manager Email"),'</label></div>
+					<div><input type="text" defaultvalue="',$config->defaults["FacMgrMail"],'" name="FacMgrMail" value="',$config->ParameterArray["FacMgrMail"],'"></div>
+				</div>
+			</div> <!-- end table -->
+		</div>
+		<div id="reporting">
+			<div class="table">
+				<div>
+					<div><label for="annualCostPerUYear">',_("Annual Cost Per Rack Unit (Year)"),'</label></div>
+					<div><input type="text" defaultvalue="',$config->defaults["annualCostPerUYear"],'" name="annualCostPerUYear" value="',$config->ParameterArray["annualCostPerUYear"],'"></div>
+				</div>
+				<div>
+					<div><label for="annualCostPerWattYear">',_("Annual Cost Per Watt (Year)"),'</label></div>
+					<div><input type="text" defaultvalue="',$config->defaults["annualCostPerWattYear"],'" name="annualCostPerWattYear" value="',$config->ParameterArray["annualCostPerWattYear"],'"></div>
+				</div>
+				<div>
+					<div><label for="PDFLogoFile">',_("Logo file for headers"),'</label></div>
+					<div><input type="text" defaultvalue="',$config->defaults["PDFLogoFile"],'" name="PDFLogoFile" value="',$config->ParameterArray["PDFLogoFile"],'"></div>
+				</div>
+				<div>
+					<div><label for="PDFfont">',_("Font"),'</label></div>
+					<div><input type="text" defaultvalue="',$config->defaults["PDFfont"],'" name="PDFfont" value="',$config->ParameterArray["PDFfont"],'"></div>
+				</div>
+			</div> <!-- end table -->
+		</div>
+	</div>';
+
 ?>
+
+<div class="table centermargin">
 <div>
-   <div></div>
-   <div><input type="submit" name="action" value="Update"></div>
-   <div><input type="button" value="Revert All To Default" onclick="javascript:verifyRevert(this.form, 'none')"></div>
-   <div></div>
+	<div>&nbsp;</div>
 </div>
-<div class="caption">
-   <a href="index.php">Return to Main Menu</a>
+<div>
+   <div><input type="submit" name="action" value="Update"></div>
 </div>
 </div> <!-- END div.table -->
 </div>
 </form>
 </div>
+   <a href="index.php">Return to Main Menu</a>
+  </div>
+  </div>
 </body>
 </html>
