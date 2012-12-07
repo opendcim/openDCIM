a7e4b107eb00b16870acf14064a2b31b2b1a5ec2
wilpig <wilbur@wilpig.org>
2012-12-07 03:21:06 -0600

    Temporary fix for version eating bug in the installer.

--- install.php
+++ install.php
@@ -452,6 +452,7 @@ if(isset($results)){
 <form action="<?php print $_SERVER["PHP_SELF"]; ?>?conf" method="POST">
    <input type="hidden" name="Revert" id="Revert" value="no">
    <input type="hidden" name="Single" id="Single" value = "none">
+   <input type="hidden" name="Version" id="Version" value = "<?php print $config->ParameterArray["Version"]; ?>">
 <div class="table rights">
 <div><div><h3>Parameter</h3></div><div><h3>Value</h3></div></div>
 <?php
