5077593975fd5a4b124dc795b9f47f5322f6a30c
wilpig <wilbur@wilpig.org>
2013-01-24 01:10:15 -0600

    Missed an instance of .live()

--- power_pdu.php
+++ power_pdu.php
@@ -211,6 +211,7 @@
 
   <script type="text/javascript" src="scripts/jquery.min.js"></script>
   <script type="text/javascript" src="scripts/jquery-ui.min.js"></script>
+  <script type="text/javascript" src="scripts/jquery-migrate-1.0.0.js"></script>
   <script type="text/javascript">
 
 	$(document).ready(function() {
