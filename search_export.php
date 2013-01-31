f53d94d2bde6e0f3ac0c7925bfc25cc573d0bfe3
wilpig <wilbur@wilpig.org>
2013-01-31 05:58:37 -0600

    Rework of search_export to make it accept datacenter lookups from a link as well as added in ajax updates for the tables to reduce page speed load time.  #101

--- search_export.php
+++ search_export.php
@@ -1,12 +1,69 @@
 <?php
-	require_once( 'db.inc.php' );
-	require_once( 'facilities.inc.php' );
+	require_once('db.inc.php');
+	require_once('facilities.inc.php');
+
+	$user=new User();
+	$user->UserID=$_SERVER['REMOTE_USER'];
+	$user->GetUserRights($facDB);
+
+	if(!$user->ReadAccess){
+		// No soup for you.
+		header('Location: '.redirect());
+		exit;
+	}
 	
-	$datacenter = new DataCenter();
-	$dcList = $datacenter->GetDCList( $facDB );
-	
-	
-	
+	$datacenter=new DataCenter();
+	$dcList=$datacenter->GetDCList($facDB);
+	$body="";
+
+	if(isset($_REQUEST['datacenterid'])){
+		$dc=isset($_POST['datacenterid'])?$_POST['datacenterid']:$_GET['datacenterid'];
+		if($dc!=''){
+			$dc=intval($dc);
+			if($dc==0){
+				$sql="select a.Name as DataCenter, e.Location, b.Position, b.Height, b.Label, b.DeviceType, c.Model, d.Name as Department, b.InstallDate from fac_DataCenter a, fac_Device b, fac_DeviceTemplate c, fac_Department d, fac_Cabinet e where b.Cabinet=e.CabinetID and e.DataCenterID=a.DataCenterID and b.TemplateID=c.TemplateID and b.Owner=d.DeptID order by DataCenter ASC, Location ASC, Position ASC";
+			}else{
+				$sql="select a.Name as DataCenter, e.Location, b.Position, b.Height, b.Label, b.DeviceType, c.Model, d.Name as Department, b.InstallDate from fac_DataCenter a, fac_Device b, fac_DeviceTemplate c, fac_Department d, fac_Cabinet e where b.Cabinet=e.CabinetID and e.DataCenterID=a.DataCenterID and b.TemplateID=c.TemplateID and b.Owner=d.DeptID and e.DataCenterID=$dc order by Location ASC, Position ASC";
+			}
+			$result=mysql_query($sql,$facDB);
+		}else{
+			$result=array();
+		}
+
+		// Left these expanded in case we need to add or remove columns.  Otherwise I would have just collapsed entirely.
+		$body="<table id=\"export\" class=\"display\">\n\t<thead>\n\t\t<tr>\n
+			\t<th>Data Center</th>
+			\t<th>Location</th>
+			\t<th>Position</th>
+			\t<th>Height</th>
+			\t<th>Name</th>
+			\t<th>Device Type</th>
+			\t<th>Template</th>
+			\t<th>Owner</th>
+			\t<th>Installation Date</th>
+			</tr>\n\t</thead>\n\t<tbody>\n";
+
+		// suppressing errors for when there is a fake data set in place
+		while($row=@mysql_fetch_array($result)){
+			// insert date formating later for regionalization settings
+			$date=date("d M Y",strtotime($row["InstallDate"]));
+			$body.="\t\t<tr>
+			\t<td>{$row["DataCenter"]}</td>
+			\t<td>{$row["Location"]}</td>
+			\t<td>{$row["Position"]}</td>
+			\t<td>{$row["Height"]}</td>
+			\t<td>{$row["Label"]}</td>
+			\t<td>{$row["DeviceType"]}</td>
+			\t<td>{$row["Model"]}</td>
+			\t<td>{$row["Department"]}</td>
+			\t<td>{$date}</td>\n\t\t</tr>\n";
+		}
+		$body.="\t\t</tbody>\n\t</table>\n";
+		if(isset($_REQUEST['ajax'])){
+			echo $body;
+			exit;
+		}
+	}
 ?>
 <!doctype html>
 <html>
@@ -16,8 +73,6 @@
   <link rel="stylesheet" href="css/inventory.php" type="text/css">
   <link rel="stylesheet" href="css/print.css" type="text/css" media="print">
   <link rel="stylesheet" href="css/jquery-ui.css" type="text/css">
-  <link rel="stylesheet" href="css/validationEngine.jquery.css" type="text/css">
-  <link rel="stylesheet" href="css/jHtmlArea.css" type="text/css">
   <link rel="stylesheet" href="css/jquery.dataTables.css" type="text/css">
   <link rel="stylesheet" href="css/ColVis.css" type="text/css">
   <link rel="stylesheet" href="css/TableTools.css" type="text/css">
@@ -28,97 +83,55 @@
   <script type="text/javascript" src="scripts/jquery.min.js"></script>
   <script type="text/javascript" src="scripts/jquery-ui.min.js"></script>
   <script type="text/javascript" src="scripts/jquery-migrate-1.0.0.js"></script>
-  <script type="text/javascript" src="scripts/mdetect.js"></script>
-  <script type="text/javascript" src="scripts/jquery.validationEngine-en.js"></script>
-  <script type="text/javascript" src="scripts/jquery.validationEngine.js"></script>
-  <script type="text/javascript" src="scripts/jHtmlArea-0.7.5.min.js"></script>
-  <script type="text/javascript" src="scripts/jquery.textext.js"></script>
   <script type="text/javascript" src="scripts/jquery.dataTables.min.js"></script>
   <script type="text/javascript" src="scripts/ColVis.min.js"></script>
   <script type="text/javascript" src="scripts/TableTools.min.js"></script>
   
-</head>
-<div id="header"></div>
-<div>
-<div class="main">
-<h2>openDCIM</h2>
-<h3>Data Center View/Export</h3>
-<?php
-if (!isset($_REQUEST['datacenterid'])) {
-	printf( "<form action=\"%s\" method=\"post\">", $_SERVER['PHP_SELF'] );
-	printf( "<table align=\"center\" border=0>" );
-
-	if ( @$_REQUEST['datacenterid'] == 0 ) {
-		printf( "<tr><td>Data Center:</td><td>\n" );
-		printf( "<select name=\"datacenterid\" onChange=\"form.submit()\">\n" );
-		printf( "<option value=\"\">Select data center</option>\n" );
-		printf( "<option value=\"0\">All Data Centers</option>\n" );
-		
-		foreach ( $dcList as $dc )
-			printf( "<option value=\"%d\">%s</option>\n", $dc->DataCenterID, $dc->Name );
-		
-		printf( "</td></tr>" );
-		
-		printf( "</table>\n</form>\n" );
-	}
-} else {
-		$dc = $_REQUEST['datacenterid'];
-
-		if ( intval( $dc ) == 0 )
-			$sql = "select a.Name as DataCenter, e.Location, b.Position, b.Height, b.Label, b.DeviceType, c.Model, d.Name as Department, b.InstallDate from fac_DataCenter a, fac_Device b, fac_DeviceTemplate c, fac_Department d, fac_Cabinet e where b.Cabinet=e.CabinetID and e.DataCenterID=a.DataCenterID and b.TemplateID=c.TemplateID and b.Owner=d.DeptID order by DataCenter ASC, Location ASC, Position ASC";
-		else 
-			$sql = sprintf( "select a.Name as DataCenter, e.Location, b.Position, b.Height, b.Label, b.DeviceType, c.Model, d.Name as Department, b.InstallDate from fac_DataCenter a, fac_Device b, fac_DeviceTemplate c, fac_Department d, fac_Cabinet e where b.Cabinet=e.CabinetID and e.DataCenterID=a.DataCenterID and b.TemplateID=c.TemplateID and b.Owner=d.DeptID and e.DataCenterID=%d order by Location ASC, Position ASC", $dc );
-
-		$result = mysql_query( $sql, $facDB );
-?>
-<table id="export" class="display">
-	<thead>
-		<tr>
-			<th>Data Center</th>
-			<th>Location</th>
-			<th>Position</th>
-			<th>Height</th>
-			<th>Name</th>
-			<th>Device Type</th>
-			<th>Template</th>
-			<th>Owner</th>
-			<th>Installation Date</th>
-		</tr>
-	</thead>
-	<tbody>
-<?php
-	while ( $row = mysql_fetch_array( $result ) ) {
-		printf( "\t<tr>\n" );
-		printf( "\t\t<td>%s</td>\n", $row["DataCenter"] );
-		printf( "\t\t<td>%s</td>\n", $row["Location"] );
-		printf( "\t\t<td>%s</td>\n", $row["Position"] );
-		printf( "\t\t<td>%s</td>\n", $row["Height"] );
-		printf( "\t\t<td>%s</td>\n", $row["Label"] );
-		printf( "\t\t<td>%s</td>\n", $row["DeviceType"] );
-		printf( "\t\t<td>%s</td>\n", $row["Model"] );
-		printf( "\t\t<td>%s</td>\n", $row["Department"] );
-		printf( "\t\t<td>%s</td>\n", date( "d M Y", strtotime( $row["InstallDate"] ) ) );
-		printf( "\t</tr>\n" );
-	}
-?>
-	</tbody>
-</table>
-
-<script type="text/javascript">
-$(document).ready( function() {
-	$( '#export' ).dataTable( {
-		"iDisplayLength" : 25,
-		"sDom" : 'CT<"clear">lfrtip',
-		"oTableTools": {
-			"sSwfPath": "scripts/copy_csv_xls.swf",
-			"aButtons": [ "copy", "csv", "xls", "print" ]
+  <script type="text/javascript">
+	$(document).ready(function(){
+		var rows;
+		function dt(){
+			$('#export').dataTable({
+				"iDisplayLength": 25,
+				"sDom": 'CT<"clear">lfrtip',
+				"oTableTools": {
+					"sSwfPath": "scripts/copy_csv_xls.swf",
+					"aButtons": ["copy","csv","xls","print"]
+				}
+			});
 		}
-	} );
-} );
-</script>
+		dt();
+		$('#datacenterid').change(function(){
+			$.post('', {datacenterid: $(this).val(), ajax: ''}, function(data){
+				$('#tablecontainer').html(data);
+				dt();
+			});
+		});
+	});
+  </script>
+</head>
+<body>
+	<div id="header"></div>
+	<div class="page">
 <?php
-}
+	include('sidebar.inc.php');
 ?>
-</div><!-- END div.page -->
+		<div class="main">
+			<h2><?php echo $config->ParameterArray['OrgName']; ?></h2>
+			<h3>Data Center View/Export</h3>
+			<label for="datacenterid">Data Center:</label>
+			<select name="datacenterid" id="datacenterid">
+				<option value="">Select data center</option>
+				<option value="0">All Data Centers</option>
+<?php foreach($dcList as $dc){print "\t\t\t\t<option value=\"$dc->DataCenterID\">$dc->Name</option>\n";} ?>
+			</select>
+			<br><br>
+			<div class="center">
+				<div id="tablecontainer">
+<?php echo $body; ?>
+				</div>
+			</div>
+		</div><!-- END div.main -->
+	</div><!-- END div.page -->
 </body>
 </html>
