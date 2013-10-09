662142fd093044d752d83134cc0850c69ae4612c
Jesse becker <hawson@gmail.com>
2013-10-08 22:46:26 -0400

    Fix #251:  Add 0.1 multipler for power calculations.

--- db-3.0-to-3.1.sql
+++ db-3.0-to-3.1.sql
@@ -1,4 +1,15 @@
 --
+-- Extend power multiplier enum
+-- An add/copy/drop has to be done to make sure that the enum 
+-- values are migrated "by value" instead of "by order"
+--
+
+ALTER TABLE fac_CDUTemplate ADD COLUMN new_mult enum('0.1','1','10','100');
+UPDATE fac_CDUTemplate SET new_mult=Multiplier;
+ALTER TABLE fac_CDUTemplate DROP Multiplier;
+ALTER TABLE fac_CDUTemplate CHANGE new_mult Multiplier enum('0.1','1','10','100');
+
+--
 -- Add coordinates and zoom defining zone in DC image
 --
 
