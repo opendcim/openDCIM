780e0c9dc85cfe66ce8ed7f8203f38724ba653f5
wilpig <wilbur@wilpig.org>
2014-03-14 12:28:17 -0500

    Slight style change #368

--- css/inventory.php
+++ css/inventory.php
@@ -623,7 +623,7 @@ div.cabinet {
 
 .device .path div { border: 0px none; }
 .device .path > div > div { position: relative; height: 1em; }
-.device .path > div > div > div { position: absolute; top: 0; min-width: 550px; padding-left: 25px; white-space: nowrap;}
+.device .path > div > div > div { position: absolute; top: 0.15em; min-width: 550px; padding-left: 25px; white-space: nowrap; font-weight: 100; font-size: 0.85em;}
 .device .path span:after{ content: " -> ";}
 .device .path span:last-child:after{ content: "";}
 
