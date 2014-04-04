46fb50a206b2732adf9ebecc194dab1ab2dc24c0
wilpig <wilbur@wilpig.org>
2014-04-04 09:26:59 -0400

    Corrections for the file uploads.  Abandoned the document root and went pure relative like I should have from the start.

--- scripts/check-exists.php
+++ scripts/check-exists.php
@@ -8,7 +8,7 @@ Copyright (c) 2012 Reactive Apps, Ronnie Garcia
 $validDir=array('pictures','drawings');
 $targetFolder=(isset($_POST['dir']) && in_array($_POST['dir'], $validDir))?$_POST['dir']:'';
 
-if (file_exists($_SERVER['DOCUMENT_ROOT'].((in_array(substr($_SERVER['DOCUMENT_ROOT'], -1), array('/',"\\")))?'':DIRECTORY_SEPARATOR).$targetFolder.DIRECTORY_SEPARATOR.$_POST['filename'])) {
+if (file_exists('..'.DIRECTORY_SEPARATOR.$targetFolder.DIRECTORY_SEPARATOR.$_POST['filename'])) {
 	echo 1;
 } else {
 	echo 0;
