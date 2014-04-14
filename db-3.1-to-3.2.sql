f297b9d1198a1cf2d4d6342a9f4fe1b1b83f2bf2
wilpig <wilbur@wilpig.org>
2014-04-14 16:27:50 -0400

    This is what happens when people try to get fancy and make several changes at once.  We should have tested this better. close #430

--- db-3.1-to-3.2.sql
+++ db-3.1-to-3.2.sql
@@ -6,11 +6,10 @@ ALTER TABLE fac_CabRow ADD COLUMN CabOrder ENUM( 'ASC', 'DESC' ) NOT NULL DEFAUL
 --
 -- Add the picture fields for front/rear views, and front/rear slots in device template 
 --
-ALTER TABLE fac_DeviceTemplate
-	ADD COLUMN FrontPictureFile VARCHAR(45) NOT NULL AFTER Notes,
-	ADD COLUMN RearPictureFile VARCHAR(45) NOT NULL AFTER FrontPictureFile,
-	ADD COLUMN ChassisSlots SMALLINT(6) NOT NULL AFTER RearPictureFile,
-	ADD COLUMN RearChassisSlots SMALLINT(6) NOT NULL AFTER ChassisSlots;
+ALTER TABLE fac_DeviceTemplate ADD COLUMN FrontPictureFile VARCHAR(45) NOT NULL AFTER Notes;
+ALTER TABLE fac_DeviceTemplate ADD COLUMN RearPictureFile VARCHAR(45) NOT NULL AFTER FrontPictureFile;
+ALTER TABLE fac_DeviceTemplate ADD COLUMN ChassisSlots SMALLINT(6) NOT NULL AFTER RearPictureFile;
+ALTER TABLE fac_DeviceTemplate ADD COLUMN RearChassisSlots SMALLINT(6) NOT NULL AFTER ChassisSlots;
 
 --
 -- Slots table content the coodinates os slots in a picture of a chassis device template 
