63871cb9ea2d1cc8010b499c10a3f2dc61b61dca
wilpig <wilbur@wilpig.org>
2014-04-26 16:12:15 -0400

    Still haven't heard back from the guy that made the uploadifive script but I was able to track down the problem to an asyncronous post operation so I forced it with an .ajax hack.  Seems to be working correctly now.  Fuck yeah! close #400

--- image_management.php
+++ image_management.php
@@ -41,7 +41,7 @@
   
   <script type="text/javascript" src="scripts/jquery.min.js"></script>
   <script type="text/javascript" src="scripts/jquery-ui.min.js"></script>
-  <script type="text/javascript" src="scripts/jquery.uploadifive.min.js"></script>
+  <script type="text/javascript" src="scripts/jquery.uploadifive.js"></script>
   <script type="text/javascript" src="scripts/common.js"></script>
 </head>
 <body>
--- scripts/check-exists.php
+++ scripts/check-exists.php
@@ -7,8 +7,9 @@ Copyright (c) 2012 Reactive Apps, Ronnie Garcia
 // Define a destination
 $validDir=array('pictures','drawings');
 $targetFolder=(isset($_POST['dir']) && in_array($_POST['dir'], $validDir))?$_POST['dir']:'';
+$targetFile=str_replace(' ','_',$_POST['filename']);
 
-if (file_exists('..'.DIRECTORY_SEPARATOR.$targetFolder.DIRECTORY_SEPARATOR.$_POST['filename'])) {
+if (file_exists('..'.DIRECTORY_SEPARATOR.$targetFolder.DIRECTORY_SEPARATOR.$targetFile)) {
 	echo 1;
 } else {
 	echo 0;
