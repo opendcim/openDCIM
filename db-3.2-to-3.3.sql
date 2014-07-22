048e459d4a9554dc0d5bb5daae6497a327ca57d5
samilliken <scott@themillikens.com>
2014-07-22 10:12:06 -0400

    Hack of a rename from 3.2-to-4.0.sql to 3.2-to-3.3.sql

---
+++ db-3.2-to-3.3.sql
@@ -0,0 +1,20 @@
+--
+-- Allow for alphanumeric names of power connections
+--
+ALTER TABLE fac_PowerConnection CHANGE PDUPosition PDUPosition VARCHAR(11) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL;
+
+--
+-- Add Config item for the Work Order Builder
+--
+INSERT INTO fac_Config set Parameter='WorkOrderBuilder', Value='disabled', UnitOfMeasure='Enabled/Disabled', ValType='string', DefaultVal='disabled';
+INSERT INTO fac_Config set Parameter='RackRequests', Value='disabled', UnitOfMeasure='Enabled/Disabled', ValType='string', DefaultVal='disabled';
+
+--
+-- Add mUnits for the temperature sensors
+--
+ALTER TABLE fac_SensorTemplate ADD COLUMN mUnits ENUM( 'english', 'metric' ) NOT NULL DEFAULT 'english';
+
+---
+--- Add dot to fac_Config to handle network map reporting
+---
+INSERT INTO fac_Config VALUES ('dot', '/usr/bin/dot', 'path', 'string', '/usr/bin/dot');
