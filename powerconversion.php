c390a4cf3fff66227b331788769c1e8de2f5b02d
wilpig <wilbur@wilpig.org>
2014-08-17 12:50:18 -0400

    Power port conversion complete, I think. #235

--- powerconversion.php
+++ powerconversion.php
@@ -13,6 +13,8 @@
 	$ConvertedCDUs=array();
 	// Store a list of the cdu template ids and the number of power connections they support
 	$CDUTemplates=array();
+	// List of ports we are going to create for every device in the system
+	$PowerPorts=array();
 
 	// Create new devices from existing CDUs
 	$sql="SELECT * FROM fac_PowerDistribution;";
@@ -33,16 +35,48 @@
 		$dev->PowerSupplyCount;
 		$dev->DeviceType='CDU';
 		$ConvertedCDUs[$row['PDUID']]=$dev->CreateDevice();
+	}
 
+	// Create list of all ports that we need to create, no need to look at children or any device with no defined power supplies
+	$sql="SELECT DeviceID, PowerSupplyCount FROM fac_Device WHERE ParentDevice=0 AND PowerSupplyCount>0;";
+	foreach($dbh->query($sql) as $row){
+		for($x=1;$x<=$row['PowerSupplyCount'];$x++){
+			$PowerPorts[$row['DeviceID']][$x]['label']=$x;
+		}
 	}
 
 	// We need to get a list of all existing power connections
-
 	$sql="SELECT * FROM fac_PowerConnection;";
 	foreach($dbh->query($sql) as $row){
-		$PowerPort=new stdClass();
-		$PowerPort->ConnectedDeviceID=$ConvertedCDUs[$row['PDUID']];
-		$PowerPort->ConnectedPort=$row['PDUPosition'];
-		$PowerPort->DeviceID=$row['DeviceID'];
-		$PowerPort->PortNumber=$row['DeviceConnNumber'];
+		// Create primary connections
+		$PowerPorts[$row['DeviceID']][$row['DeviceConnNumber']]['ConnectedDeviceID']=$ConvertedCDUs[$row['PDUID']];
+		$PowerPorts[$row['DeviceID']][$row['DeviceConnNumber']]['ConnectedPort']=$row['PDUPosition'];
+
+		// Create reverse of primary
+		$PowerPorts[$ConvertedCDUs[$row['PDUID']]][$row['PDUPosition']]['ConnectedDeviceID']=$row['DeviceID'];
+		$PowerPorts[$ConvertedCDUs[$row['PDUID']]][$row['PDUPosition']]['ConnectedPort']=$row['DeviceConnNumber'];
 	}
+
+//	print_r($PowerPorts);
+	$n=1; $insertsql=''; $insertlimit=100;
+	foreach($PowerPorts as $DeviceID => $PowerPort){
+		foreach($PowerPort as $PortNum => $PortDetails){
+			$label=(isset($PortDetails['label']))?$PortDetails['label']:$PortNum;
+			$cdevice=(isset($PortDetails['ConnectedDeviceID']))?$PortDetails['ConnectedDeviceID']:'NULL';
+			$cport=(isset($PortDetails['ConnectedPort']))?$PortDetails['ConnectedPort']:'NULL';
+
+			$insertsql.="($DeviceID,$PortNum,\"$label\",$cdevice,$cport,\"\")";
+			if($n%$insertlimit!=0){
+				$insertsql.=" ,";
+			}else{
+echo $insertsql;
+				$dbh->exec('INSERT INTO fac_PowerPorts VALUES'.$insertsql);
+				$insertsql='';
+			}
+			$n++;
+		}
+	}
+	//do one last insert
+	$insertsql=substr($insertsql, 0, -1);// shave off that last comma
+echo $insertsql;
+	$dbh->exec('INSERT INTO fac_PowerPorts VALUES'.$insertsql);
