d4a3776a034fd3b5df9a9e8c6031ce4a6297e7cd
wilpig <wilbur@wilpig.org>
2015-06-18 08:43:29 -0400

    Found another enum field that needs to be updated and a command in the installer that was left hanging.

--- db-4.0-to-4.1.sql
+++ db-4.0-to-4.1.sql
@@ -2,12 +2,16 @@
 -- Add new fields for Cabinet Order support
 --
 
-ALTER TABLE fac_Cabinet ADD COLUMN U1Position VARCHAR(7) NOT NULL DEFAULT 'Default' AFTER Notes;
+ALTER TABLE fac_Cabinet ADD COLUMN U1Position VARCHAR(7) NOT NULL DEFAULT "Default" AFTER Notes;
+ALTER TABLE fac_DataCenter ADD COLUMN U1Position VARCHAR(7) NOT NULL DEFAULT "Default" AFTER MapY;
+INSERT INTO fac_Config SET Parameter="U1Position", Value="Bottom", UnitOfMeasure="Top/Bottom", ValType="string", DefaultVal="Bottom";
 
-ALTER TABLE fac_DataCenter ADD COLUMN U1Position VARCHAR(7) NOT NULL DEFAULT 'Default' AFTER MapY;
+--
+-- Compatability updates below
+--
 
-INSERT INTO fac_Config set Parameter='U1Position', Value='Bottom', UnitOfMeasure='Top/Bottom', ValType='string', DefaultVal='Bottom';
-
+ALTER TABLE fac_SensorTemplate CHANGE mUnits mUnits VARCHAR( 7 ) NOT NULL DEFAULT "english";
+ALTER TABLE fac_SensorTemplate DROP COLUMN SNMPVersion;
 --
 -- Bump up the database version
 --
