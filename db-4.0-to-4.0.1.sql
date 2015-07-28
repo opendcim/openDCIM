ede836281d68f79425e946b37a3fe9b249057047
spezialist1 <spezialist@email.ua>
2015-07-28 17:38:54 +0300

    Insert default limits for Patch panel Ports Usage and expand FrontEdge for new conditioner air flows...
    Insert default limits for Patch panel Ports Usage and expand FrontEdge for new conditioner air flows...

--- db-4.0-to-4.0.1.sql
+++ db-4.0-to-4.0.1.sql
@@ -24,3 +24,14 @@ INSERT INTO fac_Config set Parameter='SNMPVersion', Value='2c', UnitOfMeasure='V
 -- Bump up the database version
 --
 UPDATE fac_Config set Value='4.0.1' WHERE Parameter='Version';
+
+--
+-- Insert default limits for Patch panel Ports Usage
+--
+INSERT INTO fac_Config set Parameter="PanelPortsRed", Value="80", UnitOfMeasure="percentage", ValType="float", DefaultVal="80";
+INSERT INTO fac_Config set Parameter="PanelPortsYellow", Value="60", UnitOfMeasure="percentage", ValType="float", DefaultVal="60";
+
+--
+-- Expand FrontEdge for new conditioner air flows
+--
+ALTER TABLE fac_Cabinet CHANGE FrontEdge FrontEdge VARCHAR(20) NOT NULL DEFAULT "Top";
