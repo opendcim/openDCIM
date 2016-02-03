eb77fcf7e92581004c2be29fd37fb67332a21b82
wilpig <wilbur@wilpig.org>
2016-02-03 09:33:39 -0600

    Correction to path layouts
    This shoudl be a fix for #724 but I need to test it more to make sure this didn't hose something else up.

--- paths.php
+++ paths.php
@@ -147,7 +147,7 @@
 				$pathid=$_POST['pathid'];
 			}
 			
-			
+			// Need to santize this $pathid, this is a sql injection point 2016-02-03 pig
 			$sql="SELECT DeviceID, PortNumber FROM fac_Ports WHERE Notes=\"$pathid\"";
 
 			$result = $dbh->prepare($sql);
@@ -210,8 +210,7 @@
 					//Can the path continue?
 					if ($dev->DeviceType=="Patch Panel"){
 						$path.="\n\t\t<td class=\"connection-$tipo_con-1\">";
-					}
-					else{
+					}else{
 						$path.="\n\t\t<td>";
 					}
 				
@@ -255,10 +254,10 @@
 					$path.=str_repeat("\t",$t--)."</tr>\n";
 					
 					//Ending device table
-					for ($i=sizeof($devList); $i>2; $i--){
+					for ($i=sizeof($devList); $i>1; $i--){
+						$path.=str_repeat("\t",$t--)."</table>\n";
 						$path.=str_repeat("\t",$t--)."</td>\n";
 						$path.=str_repeat("\t",$t--)."</tr>\n";
-						$path.=str_repeat("\t",$t--)."</table>\n";
 					}
 					if ($cp->PortNumber>0){
 						$t++;
