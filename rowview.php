40b69dc474ee40bf7a49e20c011d1386e2cfb145
phantium <phantium@users.noreply.github.com>
2016-02-09 14:47:30 +0100

    Fix rowview does not show reserved device
    Added !important to CSS for reserved class as the department class went before reserved causing the reserved color not to be shown.

--- rowview.php
+++ rowview.php
@@ -32,7 +32,7 @@
 
 		if($config->ParameterArray["ReservedColor"] != "#FFFFFF" || $config->ParameterArray["FreeSpaceColor"] != "#FFFFFF"){
 			$head.="		<style type=\"text/css\">
-			.reserved{background-color: {$config->ParameterArray['ReservedColor']};}
+			.reserved{background-color: {$config->ParameterArray['ReservedColor']} !important;}
 			.freespace{background-color: {$config->ParameterArray['FreeSpaceColor']};}\n";
 		}
 
