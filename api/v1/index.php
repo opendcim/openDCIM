961ccc69fb4b0b285e256783913a96f3b2950501
samilliken <scott@themillikens.com>
2016-03-02 13:49:04 -0500

    Added in the authentication middleware for APIKey checks in the API (globally set, invoked if AUTHENTICATION==LDAP).
    Requires two specific headers
    APIKey
    UserID
    
    #739

--- api/v1/index.php
+++ api/v1/index.php
@@ -1,5 +1,14 @@
 <?php
+
 	require_once( "../../db.inc.php" );
+
+	// Not really a loginPage, but this variable will keep us from getting redirected when
+	// using LDAP auth and there's no session (so true RESTful API capability)
+	//
+	if ( AUTHENTICATION == "LDAP" ) {
+		$loginPage = true;
+	}
+
 	require_once( "../../facilities.inc.php" );
 	require_once( "../../Slim/Slim.php" );
 	
@@ -92,44 +101,38 @@ function echoResponse($status_code, $response) {
  * Adding Middle Layer to authenticate every request
  * Checking if the request has valid api key in the 'Authorization' header
  */
-function authenticate(\Slim\Route $route) {
-    // Getting request headers
-    $headers = apache_request_headers();
-    $response = array();
-    $app = \Slim\Slim::getInstance();
- 
-    // Verifying Authorization Header
-    if (isset($headers['Authorization'])) {
-        // get the api key
-        $apikey = $headers['Authorization'];
-        // validating api key
-		
-		/*
-        if (!APIKey::isValidKey($api_key)) {
-            // api key is not present in users table
-            $response["error"] = true;
-            $response["message"] = "Access Denied. Invalid Api key";
-            echoResponse(401, $response);
-            $app->stop();
-        } else {
-            global $user_id;
-            // get user primary key id
-            $user = APIKey::getUserId($api_key);
-            if ($user != NULL)
-                $user_id = $user["id"];
-        }
-		*/
-		
-		global $user_id;
-		$user_id='dcim';
-    } else {
-        // api key is missing in header
-        $response["error"] = true;
-        $response["message"] = "Api key is misssing";
-        echoResponse(400, $response);
-        $app->stop();
-    }
-}
+$app->hook('slim.before.dispatch', function () {
+	if ( AUTHENTICATION == "LDAP" ) {
+	    // Getting request headers
+	    $headers = apache_request_headers();
+	    $response = array();
+	    $app = \Slim\Slim::getInstance();
+
+	    $valid = false;
+	 
+	    // Verifying Authorization Header
+	    if ( isset($headers['UserID']) && isset($headers['APIKey'])) {
+	    	// Load up the $person variable - so at this point, everything else functions
+	    	// the same way as with Apache authorization - using the $person class
+	    	global $person;
+
+	    	$person->UserID = $headers['UserID'];
+	    	$person->GetPersonByUserID();
+
+	    	if ( $person->APIKey == $headers['APIKey'] ) {
+	    		$valid = true;
+	    	}
+	    }
+
+	    if ( ! $valid ) {
+	        // api key is missing in header
+	        $response["error"] = true;
+	        $response["message"] = "API key is misssing or invalid";
+	        echoResponse(400, $response);
+	        $app->stop();
+	    }
+	}
+});
 
 /**
   *
