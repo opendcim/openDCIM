6146b900ad50f61c22a50cb0033dc455d698fd1c
wilpig <wilbur@wilpig.org>
2016-06-29 14:56:57 -0500

    Just pushing up some unfinished code...
    ...that has been laying around for a few months.  I should get back to this eventually and finish it but I don't have time at the moment

--- device_templates.php
+++ device_templates.php
@@ -341,6 +341,10 @@
   <link rel="stylesheet"  href="css/ie.css" type="text/css">
   <![endif]-->
   
+  <style type="text/css">
+	#deviceimages .center > div { border-width: 1px; border-style: solid; }
+  </style>
+
   <script type="text/javascript" src="scripts/jquery.min.js"></script>
   <script type="text/javascript" src="scripts/jquery-ui.min.js"></script>
   <script type="text/javascript" src="scripts/jquery.uploadifive.js"></script>
@@ -487,6 +491,9 @@
 				// Fill in the coords table
 				InsertCoordsTable(front,$(this));
 
+				// Draw the slots on the preview image
+				drawSlots();
+
 				// Open the dialog
 				Poopup(front);
 			});
@@ -544,6 +551,7 @@
 				rp_img.parent('div').hide();
 			}
 			resize();
+			drawSlots();
 		}).trigger('change');
 
 		$('#TemplateID').combobox();
@@ -1125,6 +1133,140 @@ function uploadifive() {
 		}
     });
 }
+
+// https://github.com/afriggeri/RYB
+// Functions for generating random number of colors.
+
+function drawSlots(){
+	var b, color, el, g, i, number, point, points, r, _i, _ref;
+
+	number = parseInt($('#coordstable > .table > div ~ div').length, 10);
+	points = new Points(number);
+	point = null;
+	bordercolors = [];
+	for (i = _i = 1; 1 <= number ? _i <= number : _i >= number; i = 1 <= number ? ++_i : --_i) {
+		point = points.pick(point);
+		_ref = RYB.rgb.apply(RYB, point).map(function(x) {
+			return Math.floor(255 * x);
+		}), r = _ref[0], g = _ref[1], b = _ref[2];
+		color = "rgb(" + r + ", " + g + ", " + b + ")";
+		bordercolors[i]=color;
+	}
+	// the first bordercolor element is null so pop it off
+	bordercolors.shift();
+
+	// clear out anything existing
+	$('#deviceimages .center > div').remove();
+
+	$('#coordstable > .table > div ~ div > div:first-child').each(function(){
+		var row=this.parentNode;
+		// figure out the slot we're dealing with
+		var slotnum=parseInt(this.textContent.replace(/\ .*/,''));
+		// figure out if we're front or rear
+		var fr=($(row).parentsUntil('.front').length==3)?'F':'R';
+
+		// define the image and div we're manipulating
+		var img=(fr=='F')?$('#img_FrontPictureFile'):$('#img_RearPictureFile');
+		var imgDiv=img.parent('div');
+		imgDiv.css('position','relative');
+
+		// amount to multiply the coordinate by to scale it to our image
+		var ratio=parseInt(img.width())/parseInt(img.naturalWidth());
+
+		// draw the slot
+		$('<div>').css({
+			'left':($('input[name="X'+fr+slotnum+'"]').val()*ratio)+'px',
+			'top':($('input[name="Y'+fr+slotnum+'"]').val()*ratio)+'px',
+			// 6px to account for fat borders on slots
+			'width':($('input[name="W'+fr+slotnum+'"]').val()*ratio)-2+'px',
+			'height':($('input[name="H'+fr+slotnum+'"]').val()*ratio)-2+'px',
+			'border-color':bordercolors[slotnum],
+			'padding-left':0,
+			'position':'absolute'
+		}).appendTo(imgDiv);
+
+		// color the row to match the slot.
+		$(row).css({'background-color':bordercolors[slotnum]});
+	});
+}
+
+var Points, RYB, display, generateColors, numberColors,
+  __hasProp = {}.hasOwnProperty,
+  __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor; child.__super__ = parent.prototype; return child; };
+
+RYB = {
+  white: [1, 1, 1],
+  red: [1, 0, 0],
+  yellow: [1, 1, 0],
+  blue: [0.163, 0.373, 0.6],
+  violet: [0.5, 0, 0.5],
+  green: [0, 0.66, 0.2],
+  orange: [1, 0.5, 0],
+  black: [0.2, 0.094, 0.0],
+  rgb: function(r, y, b) {
+    var i, _i, _results;
+    _results = [];
+    for (i = _i = 0; _i <= 2; i = ++_i) {
+      _results.push(RYB.white[i] * (1 - r) * (1 - b) * (1 - y) + RYB.red[i] * r * (1 - b) * (1 - y) + RYB.blue[i] * (1 - r) * b * (1 - y) + RYB.violet[i] * r * b * (1 - y) + RYB.yellow[i] * (1 - r) * (1 - b) * y + RYB.orange[i] * r * (1 - b) * y + RYB.green[i] * (1 - r) * b * y + RYB.black[i] * r * b * y);
+    }
+    return _results;
+  }
+};
+
+Points = (function(_super) {
+
+  __extends(Points, _super);
+
+  Points.name = 'Points';
+
+  function Points(number) {
+    var base, n, _i, _ref;
+    base = Math.ceil(Math.pow(number, 1 / 3));
+    for (n = _i = 0, _ref = Math.pow(base, 3); 0 <= _ref ? _i < _ref : _i > _ref; n = 0 <= _ref ? ++_i : --_i) {
+      this.push([Math.floor(n / (base * base)) / (base - 1), Math.floor(n / base % base) / (base - 1), Math.floor(n % base) / (base - 1)]);
+    }
+    this.picked = null;
+    this.plength = 0;
+  }
+
+  Points.prototype.distance = function(p1) {
+    var _this = this;
+    return [0, 1, 2].map(function(i) {
+      return Math.pow(p1[i] - _this.picked[i], 2);
+    }).reduce(function(a, b) {
+      return a + b;
+    });
+  };
+
+  Points.prototype.pick = function() {
+    var index, pick, _, _ref,
+      _this = this;
+    if (this.picked == null) {
+      pick = this.picked = this.shift();
+      this.plength = 1;
+    } else {
+      _ref = this.reduce(function(_arg, p2, i2) {
+        var d1, d2, i1;
+        i1 = _arg[0], d1 = _arg[1];
+        d2 = _this.distance(p2);
+        if (d1 < d2) {
+          return [i2, d2];
+        } else {
+          return [i1, d1];
+        }
+      }, [0, this.distance(this[0])]), index = _ref[0], _ = _ref[1];
+      pick = this.splice(index, 1)[0];
+      this.picked = [0, 1, 2].map(function(i) {
+        return (_this.plength * _this.picked[i] + pick[i]) / (_this.plength + 1);
+      });
+      this.plength++;
+    }
+    return pick;
+  };
+
+  return Points;
+
+})(Array);
 </script>
 </body>
 </html>
