071fe316b8354baa29b1e2e768e4eb13871ea83a
syndgr <david.ramsden@gmail.com>
2016-09-26 23:23:13 +0100

    Fix undefined variable warning for $person.

--- header.inc.php
+++ header.inc.php
@@ -2,12 +2,13 @@
 $header=(!isset($header))?$config->ParameterArray["OrgName"]:$header;
 $subheader=(!isset($subheader))?"":$subheader;
 $version=$config->ParameterArray["Version"];
+$userid=(!isset($person->UserID))?"openDCIM":$person->UserID;
 
 echo '
 <div id="header">
 	<span id="header1">',$header,'</span>
 	<span id="header2">',$subheader,'</span>
-	<span id="version">',$person->UserID,'/',$version,'</span>
+	<span id="version">',$userid,'/',$version,'</span>
 </div>
 ';
 ?>
