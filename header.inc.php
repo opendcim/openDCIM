8e4b3025912bfa5332f4e53c33f4550e1913a482
syndgr <david.ramsden@gmail.com>
2016-09-26 23:26:51 +0100

    Rename $userid variable to $uid so it doesn't trash use of $userid in, for example, ldap_bootstrap.php.

--- header.inc.php
+++ header.inc.php
@@ -2,13 +2,13 @@
 $header=(!isset($header))?$config->ParameterArray["OrgName"]:$header;
 $subheader=(!isset($subheader))?"":$subheader;
 $version=$config->ParameterArray["Version"];
-$userid=(!isset($person->UserID))?"openDCIM":$person->UserID;
+$uid=(!isset($person->UserID))?"openDCIM":$person->UserID;
 
 echo '
 <div id="header">
 	<span id="header1">',$header,'</span>
 	<span id="header2">',$subheader,'</span>
-	<span id="version">',$userid,'/',$version,'</span>
+	<span id="version">',$uid,'/',$version,'</span>
 </div>
 ';
 ?>
