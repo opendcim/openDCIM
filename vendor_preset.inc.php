12059c691b4d3253a51f54bdbbc9363b8726b9f5
Gary T. Giesen <ggiesen@self.giesen.me>
2017-05-31 10:18:53 -0400

    Add in missing vendor_preset_defs.inc.php file and associated refactoring of vendor_preset.inc.php

--- vendor_preset.inc.php
+++ vendor_preset.inc.php
@@ -1,16 +1,6 @@
 <?php
 
-	/**
-	* Directory where configuration files are stored.
-	* It is not used directly in code, just a convenient
-	* define used further in this file.
-	*/
-	define('CONFIG_DIR', './');
-
-	/**
-	* Filename of a configuration file.
-	*/
-	define('CONFIG_FILE', CONFIG_DIR . 'db.inc.php');
+	require_once( 'vendor_preset_defs.inc.php' );
 
 	require_once(CONFIG_FILE);
 
---
+++ vendor_preset_defs.inc.php
@@ -0,0 +1,15 @@
+<?php
+
+	/**
+	* Directory where configuration files are stored.
+	* It is not used directly in code, just a convenient
+	* define used further in this file.
+	*/
+	define('CONFIG_DIR', '/etc/opendcim/');
+
+	/**
+	* Filename of a configuration file.
+	*/
+	define('CONFIG_FILE', CONFIG_DIR . 'db.inc.php');
+
+?>
