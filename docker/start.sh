18158b352a0e30cded3e05c233bfeceedc598e35
Pouyan Heyratpour <pouyan@nested.me>
2017-09-27 04:12:49 +0330

    Fix issue in db host set
    Signed-off-by: Pouyan Heyratpour <pouyan@nested.me>

--- docker/start.sh
+++ docker/start.sh
@@ -2,5 +2,6 @@
 
 sed -i "s|.*server_name.*|	server_name $SERVER_NAME;" /etc/nginx/conf.d/site.conf
 sed -i "s|.*server_name.*|	server_name $SERVER_NAME;" /etc/nginx/conf.d/site-ssl.conf
+sed -i "s|.*\$dbhost.*=.*|	\$dbhost = '$MYSQL_HOST';" /var/www/html/db.inc.php
 
 supervisord --nodaemon --configuration /etc/supervisord.conf
