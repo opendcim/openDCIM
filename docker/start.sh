e383f4c8cae333cd74fd2c581cd26ed6b2600085
Pouyan Heyratpour <pouyan@nested.me>
2017-09-27 06:02:57 +0330

    Docker: Fix db.inc.php issue
    Signed-off-by: Pouyan Heyratpour <pouyan@nested.me>

--- docker/start.sh
+++ docker/start.sh
@@ -2,6 +2,6 @@
 
 sed -i "s|.*server_name.*|	server_name $SERVER_NAME;|" /etc/nginx/conf.d/site.conf
 sed -i "s|.*server_name.*|	server_name $SERVER_NAME;|" /etc/nginx/conf.d/site-ssl.conf
-sed -i "s|\s*\$dbhost\s*=\s*.*|	\$dbhost = '$MYSQL_HOST';|" /var/www/html/db.inc.php
+sed "s|\s*\$dbhost\s*=\s*.*|	\$dbhost = '$MYSQL_HOST';|" /var/www/html/db.inc.php-dist > /var/www/html/db.inc.php
 
 supervisord --nodaemon --configuration /etc/supervisord.conf
