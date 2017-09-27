51426880efb5c0011aaa0ea27b2b555a34fb3fcc
Pouyan Heyratpour <pouyan@nested.me>
2017-09-27 04:15:59 +0330

    Docker: Another typo in sed
    Signed-off-by: Pouyan Heyratpour <pouyan@nested.me>

--- docker/start.sh
+++ docker/start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-sed -i "s|.*server_name.*|	server_name $SERVER_NAME;" /etc/nginx/conf.d/site.conf
-sed -i "s|.*server_name.*|	server_name $SERVER_NAME;" /etc/nginx/conf.d/site-ssl.conf
-sed -i "s|.*\$dbhost.*=.*|	\$dbhost = '$MYSQL_HOST';" /var/www/html/db.inc.php
+sed -i "s|.*server_name.*|	server_name $SERVER_NAME;|" /etc/nginx/conf.d/site.conf
+sed -i "s|.*server_name.*|	server_name $SERVER_NAME;|" /etc/nginx/conf.d/site-ssl.conf
+sed -i "s|.*\$dbhost.*=.*|	\$dbhost = '$MYSQL_HOST';|" /var/www/html/db.inc.php
 
 supervisord --nodaemon --configuration /etc/supervisord.conf
