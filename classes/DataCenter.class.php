2094a07fa870d4ede1008ceaded8496076899769
Scott Milliken <scott@riverrun.home.themillikens.com>
2017-12-07 08:17:16 -0500

    Found a spot where we were double counting - infrastructure was also counting reserved devices, but the available subtracted infrastructure and reserved... shouldn't make a huge impact.  I'm sure something else is off, but wanted to commit this while I was looking at it.
    #990

--- classes/DataCenter.class.php
+++ classes/DataCenter.class.php
@@ -539,7 +539,7 @@ class DataCenter {
 		// Count the U used up by devices that are (a) not chassis cards; (b) in a cabinet in the data center; (c) not a server or array; and (d) not in the Storage Room (Cabinet=-1)
 		$sql="SELECT SUM(a.Height) as TotalU FROM fac_Device a,fac_Cabinet b WHERE 
 			a.Cabinet=b.CabinetID AND b.DataCenterID=$this->DataCenterID AND ParentDevice=0 AND
-			a.DeviceType NOT IN ('Server','Storage Array') and a.Cabinet>0;";
+			a.DeviceType NOT IN ('Server','Storage Array') and a.Status NOT IN ('Reserved', 'Salvage') and a.Cabinet>0;";
 		$dcStats["Infrastructure"]=($test=$this->query($sql)->fetchColumn())?$test:0;
  
 		$sql="SELECT SUM(a.Height) as TotalU FROM fac_Device a,fac_Cabinet b WHERE 
