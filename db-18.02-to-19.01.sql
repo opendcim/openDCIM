57817bda1b7c2192b53b06650ab5aaa7644ec9ba
samilliken <scott@themillikens.com>
2019-01-21 13:14:37 -0500

    Forgot to update the db file with the new index, which also greatly increases performance.
    #1124

--- db-18.02-to-19.01.sql
+++ db-18.02-to-19.01.sql
@@ -4,6 +4,10 @@
 
 INSERT INTO fac_Config set Parameter='logretention', Value='90', UnitOfMeasure='days', ValType='integer', DefaultVal='90';
 
+-- Add an index to the fac_GenericLog to speed it up
+
+CREATE INDEX ObjectID on fac_GenericLog (ObjectID);
+
 
 --
 -- Bump up the database version (uncomment below once released)
