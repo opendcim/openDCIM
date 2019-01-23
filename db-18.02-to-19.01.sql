30340ce0d9ea4e98fbb1d1037f93bac6811ad48b
wilpig <wilbur@wilpig.org>
2019-01-23 05:16:03 -0600

    Some turd didn't rename the 18.03 that didn't happen to 19.01

--- db-18.02-to-19.01.sql
+++ db-18.02-to-19.01.sql
@@ -1,4 +1,10 @@
 --
+-- Add configuration options for ldap debug 
+--
+INSERT INTO fac_Config (Parameter, Value, UnitOfMeasure, ValType, DefaultVal) VALUES ("LDAPDebug", "enabled", "Enabled/Disabled", "string", "disabled");
+UPDATE fac_Config SET DefaultVal="(|(uid=%userid%)(sAMAccountName=%userid%))" WHERE Parameter="LDAPUserSearch";
+
+--
 -- Add in new configuration item for log retention
 --
 
