c6a0ae67d08cfcbb88dc2453826c1c67868ca677
samilliken <scott@themillikens.com>
2019-03-03 20:00:02 -0500

    Finally have this working the way I want it.
    Fixed #1111

--- scripts/testemail.php
+++ scripts/testemail.php
@@ -2,7 +2,7 @@
 	require_once( "../db.inc.php" );
 	require_once( "../facilities.inc.php" );
 
-	header("Content-type: text/json");
+	header("Content-type: text/html");
 
 	if(isset( $_REQUEST['SMTPServer'] ) && isset( $_REQUEST['SMTPPort'] ) && isset( $_REQUEST['SMTPUser'] ) && isset( $_REQUEST['SMTPPassword'] ) && isset( $_REQUEST['FacMgrMail']) ) {
 		if ( $_REQUEST['SMTPPort'] != 25 ) {
@@ -33,9 +33,18 @@
 			$error.=__("MailFrom").": <span class=\"errmsg\">".$e->getMessage()."</span><br>\n";
 		}
 
-		$logo=getcwd().'/images/'.$config->ParameterArray["PDFLogoFile"];
-		$logo=$message->embed(Swift_Image::fromPath($logo)->setFilename('logo.png'));
-		
+		if ( isset( $_REQUEST['MailToAddr'])) {
+			$mailTo = $_REQUEST['MailToAddr'];
+		} else {
+			$mailTo = $_REQUEST['FacMgrMail'];
+		}
+
+		try{		
+			$message->addTo($mailTo);
+		}catch(Swift_RfcComplianceException $e){
+			$error.=__("MailTo").": <span class=\"errmsg\">".$e->getMessage()."</span><br>\n";
+		}
+
 		$style = "
 <style type=\"text/css\">
 @media print {
@@ -45,12 +54,11 @@
 }
 </style>";
 
-		$htmlMessage = sprintf( "<!doctype html><html><head><meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\"><title>%s</title>%s</head><body><div id=\"header\" style=\"padding: 5px 0;background: %s;\"><center><img src=\"%s\"></center></div><div class=\"page\"><p>\n", __("Data Center Inventory Reservations"), $style, $config->ParameterArray["HeaderColor"], $logo  );
+		$htmlMessage = sprintf( "<!doctype html><html><head><meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\"><title>%s</title>%s</head><body><div id=\"header\" style=\"padding: 5px 0;background: %s;\"><center></center></div><div class=\"page\"><p>\n", __("Data Center SMTP Test"), $style, $config->ParameterArray["HeaderColor"]  );
 
 		$htmlMessage .= __("<p>This is a test email sent by an administrator from the openDCIM system at") . ' ' . $_SERVER['SERVER_NAME'] . '</p>';
 
 		$message->setBody($htmlMessage,'text/html');
-
 
 		try {
 			$result = $mailer->send( $message );
