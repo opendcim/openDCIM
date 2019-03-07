4d352ba34a2c6bda1d78ecece53f503096632e0c
Timothy Legge <timlegge@gmail.com>
2019-03-07 18:30:58 -0400

    SAML Validate the response is related to the request

--- saml/login.php
+++ saml/login.php
@@ -18,6 +18,9 @@ if (!isset($_SESSION['samlUserdata'])) {
 	$settings = new OneLogin_Saml2_Settings($saml_settings);
 	$authRequest = new OneLogin_Saml2_AuthnRequest($settings);
 	$samlRequest = $authRequest->getRequest();
+
+	// Track the authRequest ID so the response can be validated
+	$_SESSION['saml_req_id'] = $authRequest->getID();
 	
 	$parameters = array('SAMLRequest' => $samlRequest);
 	$parameters['RelayState'] = OneLogin_Saml2_Utils::getSelfURLNoQuery();
