0726d585fa1663734eec76524c3ac5dedee4b86e
samilliken <scott@themillikens.com>
2020-05-21 21:50:37 -0400

    Fixed bad merge for metadata.php #1224

--- saml/metadata.php
+++ saml/metadata.php
@@ -1,38 +1,25 @@
 <?php
  
 /**
-<<<<<<< HEAD
  *  SP Metadata Endpoint
  */
 
 $loginPage = true;
 
-=======
- *  SAML Metadata view
- */
-
->>>>>>> a500912f80079b7ec4ae7414e8f675e71950c45f
 require_once('../db.inc.php');
 
 define("TOOLKIT_PATH", '../vendor/onelogin/php-saml/');
 require_once(TOOLKIT_PATH . '_toolkit_loader.php');
 
-<<<<<<< HEAD
 require_once('settings.php');
 
 try {
     $settings = new OneLogin_Saml2_Settings($saml_settings);
     $metadata = $settings->getSPMetadata($settings);
-=======
-require_once 'settings.php' ;
 
-try {
-    #$auth = new OneLogin_Saml2_Auth($settingsInfo);
-    #$settings = $auth->getSettings();
     // Now we only validate SP settings
     $settings = new OneLogin_Saml2_Settings($saml_settings, true);
     $metadata = $settings->getSPMetadata();
->>>>>>> a500912f80079b7ec4ae7414e8f675e71950c45f
     $errors = $settings->validateMetadata($metadata);
     if (empty($errors)) {
         header('Content-Type: text/xml');
