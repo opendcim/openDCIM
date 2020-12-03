a2e63011122de3573ba045102ef1c7251802202c
samilliken <scott@themillikens.com>
2020-12-03 17:19:23 -0500

    Added standalone script that can be called as cronjob or manually to refresh IdP metadata.  Fixed #1262

--- db-20.01-to-20.02.sql
+++ db-20.01-to-20.02.sql
@@ -8,4 +8,4 @@ UPDATE fac_Config SET DefaultVal = 'Location' WHERE fac_Config.Parameter = 'Assi
 -- Bump up the database version (uncomment below once released)
 --
 
--- UPDATE fac_Config set Value="20.02" WHERE Parameter="Version";
+UPDATE fac_Config set Value="20.02" WHERE Parameter="Version";
---
+++ refresh_idp_metadata.php
@@ -0,0 +1,35 @@
+<?php
+	require_once "db.inc.php";
+	require_once "facilities.inc.php";
+
+	define("TOOLKIT_PATH", './vendor/onelogin/php-saml/');
+	require_once(TOOLKIT_PATH . '_toolkit_loader.php');
+	require_once( "./saml/settings.php" );
+
+	$parser = new OneLogin\Saml2\IdPMetadataParser;
+	error_log( "Downloading new IdP Metadata from " . $config->ParameterArray["SAMLIdPMetadataURL"]);
+	$IdPSettings = $parser->parseRemoteXML($config->ParameterArray["SAMLIdPMetadataURL"]);
+
+	// Overcommunicate so that we can actually keep track of any changes in the logs
+	if ( $config->ParameterArray["SAMLidpentityId"] != $IdPSettings['idp']['entityId'] ) {
+		$config->ParameterArray["SAMLidpentityId"] = $IdPSettings['idp']['entityId'];
+		error_log( "SAMLidpentityId updated to " . $IdPSettings['idp']['entityId']);
+	}
+
+	if ( $config->ParameterArray["SAMLidpx509cert"] != $IdPSettings['idp']['x509cert'] ) {
+		$config->ParameterArray["SAMLidpx509cert"] = $IdPSettings['idp']['x509cert'];
+		error_log( "SAMLidpx509cert updated to " . $IdPSettings['idp']['x509cert']);
+	}
+	if ( $config->ParameterArray["SAMLidpslsURL"] != $IdPSettings['idp']['singleLogoutService']['url'] ) {
+		$config->ParameterArray["SAMLidpslsURL"] = $IdPSettings['idp']['singleLogoutService']['url'];
+		error_log( "SAMLidpslsURL updated to " . $IdPSettings['idp']['singleLogoutService']['url']);
+	}
+	if ( $config->ParameterArray["SAMLidpssoURL"] != $IdPSettings['idp']['singleSignOnService']['url'] ) {
+		$config->ParameterArray["SAMLidpssoURL"] = $IdPSettings['idp']['singleSignOnService']['url'];
+		error_log( "SAMLidpssoURL updated to " . $IdPSettings['idp']['singleSignOnService']['url']);
+	}
+
+	$config->UpdateConfig();
+	
+	error_log( "Metadata refresh complete." );
+?>
