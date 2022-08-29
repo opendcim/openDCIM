a531fec34249597dc24d969452429ca10a5adca7
samilliken <scott@themillikens.com>
2022-08-29 19:54:33 -0400

    Added and documented new route to pull sensors by filters.  In the process, fixed the Device->Search to actually filter the way the documentation says (including attributes of the cabinets, which it did not, before).  #1341

--- classes/Device.class.php
+++ classes/Device.class.php
@@ -1414,24 +1414,24 @@ class Device {
 		// This will store all our extended sql
 		$sqlextend="";
 		foreach($o as $prop => $val){
-			if(property_exists("Device",$prop)){
+			if(property_exists("Device",$prop) || in_array( $prop, array( "DataCenterID", "ZoneID", "CabRowID", "CabinetID" ))){
 				extendsql($prop,$this->$prop,$sqlextend,$loose);
 			}else{
 				if(array_key_exists($prop,$attrList)){
 					attribsql($attrList[$prop]->AttributeID,$val,$customSQL,$loose);
-				}else{
-					// The requested attribute is not valid.  Ain't nobody got time for that!
+				} else {
+				// The requested attribute is not valid.  Ain't nobody got time for that!
 				}
 			}
 		}
+
 		if($sqlextend==""){
-			// No base attributes to search, only custom
-			$sqlextend="WHERE TRUE";
+			$sqlextend="WHERE TRUE ";
 		}
 		if($customSQL!=""){
 			$customSQL="AND DeviceID IN (SELECT DeviceID FROM fac_DeviceCustomValue $customSQL)";
 		}
-		$sql="SELECT * FROM fac_Device $sqlextend $customSQL ORDER BY Label ASC;";
+		$sql="SELECT * FROM fac_Device, fac_Cabinet $sqlextend AND fac_Device.Cabinet=fac_Cabinet.CabinetID $customSQL ORDER BY Label ASC;";
 
 		$deviceList=array();
 
