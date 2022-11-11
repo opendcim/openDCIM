eec90d11f76ddfcee63a73894303d0771b9ba726
Jonathan Angliss <jon+gh@netdork.net>
2022-11-11 19:46:28 +0000

    Fix E_WARNINGs for proj/grp membership.

--- dept_groups.php
+++ dept_groups.php
@@ -21,7 +21,7 @@
 
 	// Update if form was submitted and action is set
 	if(isset($_POST['action']) && $_POST['action']=="Submit"){
-		$grpMembers=$_POST['chosen'];
+		$grpMembers=!empty($_POST['chosen'])?$_POST['chosen']:array();
 		$dept->AssignContacts($grpMembers);
 	}
 
--- project_members.php
+++ project_members.php
@@ -30,7 +30,7 @@
 
 	// Update if form was submitted and action is set
 	if(isset($_POST['action']) && $_POST['action']=="Submit"){
-		$grpMembers=$_POST['chosen'];
+		$grpMembers=!empty($_POST['chosen'])?$_POST['chosen']:array();
 		if (( sizeof( $grpMembers ) > 0 ) && isset( $_REQUEST['projectid'] )) {
 			$ProjectID = $_REQUEST['projectid'];
 			ProjectMembership::clearMembership( $ProjectID );
