03ec7f5a4cd6fa82cb6c83ffd277e911a267476b
Scott Milliken <scott@themillikens.com>
2023-03-07 13:24:29 -0500

    Update oidc_test.php

--- oidc_test.php
+++ oidc_test.php
@@ -4,6 +4,8 @@ require __DIR__ . "/vendor/autoload.php";
 
 use Jumbojett\OpenIDConnectClient;
 
+// This is a test lab client secret and using an OAuth installation that isn't accessible from the internet
+// once we write in the new login client this file will be removed, anyway
 $oidc = new OpenIDConnectClient('https://keycloak.cadmuslabs.net/auth/realms/openDCIM/',
                                 'dcim-live',
                                 'c0e0cd6f-65cc-45b9-9c90-7d991024f8b7');
