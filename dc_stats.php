a942c238ad811d97555d9a50c72cef686d0ca8e2
wilpig <wilbur@wilpig.org>
2023-04-12 07:39:23 -0500

    Bugfix: dc_stats image encoding
    This is the one place it seems that we used a straight path to an image as an html attribute so the path needed to be urlencoded so that the browser would parse it in the event that extended ascii are present in the path

--- dc_stats.php
+++ dc_stats.php
@@ -87,7 +87,7 @@
 	 
 		if(strlen($dc->DrawingFileName)>0){
 			$mapfile=$config->ParameterArray["drawingpath"] . $dc->DrawingFileName;
-		   
+ 
 			if(file_exists($mapfile)){
 				if(mime_content_type($mapfile)=='image/svg+xml'){
 					$svgfile = simplexml_load_file($mapfile);
@@ -96,7 +96,7 @@
 				}else{
 					list($width, $height, $type, $attr)=getimagesize($mapfile);
 				}
-				$mapHTML="<div class=\"canvas\" style=\"background-image: url('".$mapfile."')\">
+				$mapHTML="<div class=\"canvas\" style=\"background-image: url('".urlencode($mapfile)."')\">
 	<img src=\"css/blank.gif\" usemap=\"#datacenter\" width=\"$width\" height=\"$height\" alt=\"clearmap over canvas\">
 	<map name=\"datacenter\" data-dc=$dc->DataCenterID data-zoom=1 data-x1=0 data-y1=0>
 	</map>
