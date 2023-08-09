11d580ddcf99bda25b8dfb9691a63dc71bea749b
samilliken <scott@themillikens.com>
2023-08-09 16:26:56 -0400

    Converted the /api/test script over to Slim v3.  #1382

--- api/test/index.php
+++ api/test/index.php
@@ -2,7 +2,6 @@
 
 	require_once "../../vendor/autoload.php";
 	
-/* Code for when we go to Slim Framework v3	
 	use Psr\Http\Message\ServerRequestInterface as Request;
 	use Psr\Http\Message\ResponseInterface as Response;
 
@@ -15,34 +14,12 @@
 	$c = new \Slim\Container($configuration);
 	
 	$app = new \Slim\App($c);
-*/
 
-// Framework v2 Specific - we have to do our own output formatting
-function echoResponse( $response ) {
-	$app = \Slim\Slim::getInstance();
-
-	if ( array_key_exists( 'errorcode', $response )) {
-		$app->status( $response['errorcode'] );
-	}
-
-	$app->contentType( 'application/json' );
-
-	echo json_encode( $response );
-}
-
-	\Slim\Slim::registerAutoLoader();
-	$app = new \Slim\Slim();
-
-//	$app->get('/test', function( Request $in, Response $out ) {
-	$app->get( '/test', function() {
+	$app->get( '/test', function(Request $request, Response $response) {
 		$r['error'] = false;
 		$r['errorcode'] = 200;
-		/* v3 code 
-		$out = $out->withJson($r, $r['errorcode'] );
-		return $out;
-		*/
 
-		echoResponse( $r );
+		return $response->withJson($r, $r['errorcode'] );
 	});
  
 $app->run();
