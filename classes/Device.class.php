f3a909039fcfe06700f27800658ee7c643e5449d
wilpig <wilbur@wilpig.org>
2023-09-06 06:34:24 -0500

    Remove comma fixes #1434

--- classes/Device.class.php
+++ classes/Device.class.php
@@ -611,7 +611,7 @@ class Device {
 		if($this->GetDevice() && $new->GetDevice()) {
 			$sql="INSERT INTO fac_DeviceCustomValue(DeviceID, AttributeID, Value) 
 				SELECT $new->DeviceID, dcv.AttributeID, dcv.Value 
-				FROM fac_DeviceCustomValue, dcv WHERE dcv.DeviceID=$this->DeviceID
+				FROM fac_DeviceCustomValue dcv WHERE dcv.DeviceID=$this->DeviceID
 				ON DUPLICATE KEY UPDATE Value=dcv.Value;";
 
 			if(!$dbh->query($sql)){
