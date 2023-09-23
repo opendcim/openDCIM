2bbb46b1f963beb87d7841716f759a81831b824b
samilliken <scott@themillikens.com>
2023-09-23 12:37:11 -0400

    Enough for today, time to watch football.   Most of the steps are laid out, just need to keep filling them in as I have time.  #1436

--- site_merge.php
+++ site_merge.php
@@ -9,12 +9,15 @@
 	in the db.config.php file.   The child db information is to be set below and the system
 	that this script runs on will need network access to that db server.
 
-	All databases will be placed within a newly created container in the parent that matches
+	All objects will be placed within a newly created container in the parent that matches
 	the childPrefix.  After the import is completed, the container can be renamed in the UI.
 	Additional objects will also have the childPrefix inserted, such as Departments, in order
 	to avoid conflicts.  (ie - both sites have an 'Operations' department)
 
 	All assets (drawings and pictures) will need to be manually copied over on the filesystem.
+
+	Audit logs are NOT copied over, as everything imported is basically a fresh start.  If you
+	need your history from before the import, save the last db dump from the imported site.
 
 */
 
@@ -175,4 +178,173 @@ $pContainer->Name = $childPrefix;
 $pContainer->countryCode = $childCountryCode;
 $pContainer->CreateContainer();
 
-# Now start creating all of the subsequent containers, data centers, zones, and rows
+# All top level Containers in the child will now be mapped to the newly created container under the parent
+$containerMap = array();
+$containerMap[0] = $pContainer->ContainerID;
+
+# Now start creating all of the subsequent Containers
+
+$cSQL = "select * from fac_Container";
+$cStmt = $childDBH->prepare( $cSQL );
+$cStmt->execute();
+
+while ( $row = $cStmt->fetch() ) {
+	$targetC = new Container;
+	$targetC->Name = $row["Name"];
+	$targetC->DrawingFileName = $row["DrawingFileName"];
+	$targetC->MapX = $row["MapX"];
+	$targetC->MapY = $row["MapY"];
+	$targetC->ParentID = $row["ParentID"]; # This is the OLD ID, but we will reconcile after all containers are built
+	$targetC->CreateContainer();
+	error_log( "Created new Container ID of ".$targetC->ContainerID." for ".$targetC->Name );
+
+	$containerMap[$row["ContainerID"]] = $targetC->ContainerID;
+}
+
+# Reconciliation - remap the ParentID from the old value to the new one based on the mapping
+foreach( $containerMap as $oldVal=>$newVal ) {
+	$targetC->ContainerID = $newVal;
+	$targetC->GetContainer();
+	$targetC->ParentID = $containerMap[$targetC->ParentID];
+	$targetC->UpdateContainer();
+}
+
+# Next up is Data Centers
+
+$dcMap = array();
+$dcSQL = "select * from fac_DataCenter";
+$dcStmt = $childDBH->prepare( $dcSQL );
+$dcStmt->execute();
+
+while ( $row = $dcStmt->fetch() ) {
+	$targetDC = new DataCenter;
+	foreach( $row as $prop=>$value ) {
+		if ( ! in_array( $prop, array("DataCenterID", "ContainerID")) ) {
+			$targetDC->$prop = $value;
+		}
+	}
+	$targetDC->ContainerID = $containerMap[$row["ContainerID"]];
+	$targetDC->CreateDataCenter();
+	error_log( "Created new DataCenter ID of ".$targetDC->DataCenterID." for ".$targetDC->Name );
+
+	$dcMap[$row["DataCenterID"]] = $targetDC->DataCenterID;
+}
+
+# Now Zones
+
+$zMap = array();
+$zSQL = "select * from fac_Zone";
+$zStmt = $childDBH->prepare( $zSQL );
+$zStmt->execute();
+
+while ( $row = $zStmt->fetch() ) {
+	$targetZone = new Zone;
+	foreach( $row as $prop=>$value ) {
+		if ( ! in_array( $prop, array( "ZoneID", "DataCenterID" )) ) {
+			$targetZone->$prop = $value;
+		}
+	}
+	$targetZ->DataCenterID = $dcMap[$row["DataCenterID"]];
+	$targetZ->CreateZone();
+	error_log( "Created new Zone ID of ".$targetZ->ZoneID." for Zone ".$targetZ->Name );
+
+	$zMap[$row["ZoneID"]] = $targetZ->ZoneID;
+}
+
+# CabinetRows
+
+$cabrowMap = array();
+$cSQL = "select * from fac_CabRow";
+$cStmt = $childDBH->prepare( $cSQL );
+$cStmt->execute();
+
+while ( $row = $cStmt->fetch() ) {
+	$targetRow = new CabRow;
+	$targetRow->Name = $row["Name"];
+	$targetRow->DataCenterID = $dcMap[$row["DataCenterID"]];
+	$targetRow->ZoneID = $zMap[$row["ZoneID"]];
+	$targetRow->CreateCabRow();
+	error_log( "Created new CabinetRow ID of ".$targetRow->CabRowID." for Row ".$targetRow->Name );
+
+	$cabrowMap[$row["CabRowID"]] = $targetRow->CabRowID;
+}
+
+# Power Panels
+
+$ppMap = array();
+$ppSQL = "select * from fac_PowerPanel";
+$ppStmt = $childDBH->prepare( $ppSQL );
+$ppStmt->execute();
+
+while ( $row = $ppStmt->fetch() ) {
+	$targetPP = new PowerPanel;
+	foreach( $row as $prop=>$value ) {
+		if ( ! in_array( $prop, array( "PanelID", "MapDataCenterID", "PanelLabel" )) ) {
+			$targetPP->$prop = $value;
+		}
+	}
+	if ( $row["MapDataCenterID"] > 0 ) {
+		$targetPP->MapDataCenterID = $dcMap[$row["MapDataCenterID"]];
+	}
+	# Because panel numbering is fairly standard, add the prefix onto the imported ones to keep from having name conflicts
+	$targetPP->PanelLabel = $childPrefix.$row["PanelLabel"];
+
+	$targetPP->createPanel();
+	error_log( "Created new Power Panel ID of ".$targetPP->PanelID." for Panel ".$targetPP->Name );
+
+	$ppMap[$row["PanelID"]] = $targetPP->PanelID;
+}
+
+# Just like with the Containers, since the parent panel could have been made after the child, we have to reconcile after all are pulled in
+foreach( $ppMap as $oldVal=>$newVal ) {
+	$targetPP->PanelID = $newVal;
+	$targetPP->getPanel();
+	$targetPP->ParentPanelID = $ppMap[$targetPP->ParentPanelID];
+	$targetC->updatePanel();
+}
+
+# Cabinets
+
+# People
+
+# Departments and their Memberships
+
+
+# Now the nasty stuff...  Devices
+$devMap = array();
+$devSQL = "select * from fac_Device";
+$devStmt = $childDBH->preopare( $devSQL );
+$devStmt->execute();
+
+# Port Connections will have to be reconciled after ALL of the devices are created
+
+while ( $row = $devStmt->fetch() ) {
+	$targetDev = new Device;
+	foreach( $row as $prop=>$value ) {
+		if ( ! in_array( $prop, array( "DeviceID", "Owner", "PrimaryContact", "Cabinet", "TemplateID")) ) {
+			$targetDev->$prop = $value;
+		}
+	}
+	$targetDev->Owner = $deptMap[$row["Owner"]];
+	$targetDev->PrimaryContact = $peopleMap[$row["PrimaryContact"]];
+	$targetDev->Cabinet = $cabMap[$row["Cabinet"]];
+	$targetDev->TemplateID = $dtMap[$row["TemplateID"]];
+	$targetDev->CreateDevice();
+	error_log( "Created new DeviceID of ".$targetDev->DeviceID." for Device ".$targetDev->Label );
+
+	$devMap[$row["DeviceID"]] = $targetDev->DeviceID;
+}
+
+# Now reconcile
+error_log( "Beginning device reconciliation process, this could take some time." );
+
+foreach( $devMap as $oldVal=>$newVal ) {
+	$targetDev->DeviceID = $newVal;
+	$targetDev->GetDevice();
+	if ( $targetDev->ParentID > 0 ) {
+		$targetDev->ParentID = $devMap[$targetDev->ParentID];
+	}
+	$targetDev->UpdateDevice();
+
+	# Now map the Device Port and Power Port connections
+}
